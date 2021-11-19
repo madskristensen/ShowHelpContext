263a0edd649ab3d21d6174edfba4e29365c9545e
Mads Kristensen <madskristensen@users.noreply.github.com>
2021-11-19 12:58:46 -0800

    Add project files.

---
+++ src/Commands/MyCommand.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.Shell.Interop;
+
+namespace ShowHelpContext
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        public static Guid DebugHelpContextToolWindowGuid = new Guid("66dba47c-61df-11d2-aa79-00c04f990343");
+
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            IVsUIShell vsUIShell = await VS.Services.GetUIShellAsync();
+            vsUIShell.FindToolWindow((uint)__VSFINDTOOLWIN.FTW_fForceCreate, ref DebugHelpContextToolWindowGuid, out IVsWindowFrame vsWindowFrame);
+
+            if (null != vsWindowFrame)
+            {
+                vsWindowFrame.Show();
+            }
+        }
+    }
+}
